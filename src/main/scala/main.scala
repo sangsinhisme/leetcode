dcf2a8fe8256e085030f913c65079eed85b54132
Nguyen Sang Sinh <sinhns2>
2024-07-13 12:13:20 +0700

    add(day1): first resolve

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,18 @@
+import Utils._
+import Integrals._
 @main
 def main(): Unit = {
-  println("Hello world!")
+
+  val listInput = readFile("01.txt")
+  val coefficients: List[Int] = string2List[Int](elem => elem.toInt)(listInput.head)
+  val powers: List[Int] = string2List[Int](elem => elem.toInt)(listInput.take(2).last)
+  val limits: List[Int] = string2List[Int](elem => elem.toInt)(listInput.last)
+
+  for (ans <- displayAnswers(coefficients, powers, limits)) println(ans)
 }
+
+
+//def main(args: Array[String]): Unit = {
+//  /** Purely IO Section * */
+//  displayAnswers(readLine().trim().split(" ").toList.map(_.toInt), readLine().trim().split(" ").toList.map(_.toInt), readLine().trim().split(" ").toList.map(_.toInt))
+//}
