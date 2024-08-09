98b22450d0c1332168c3c24346d9a968d79095b3
sinhns2 <sangsinh789@gmail.com>
2024-08-09 16:11:06 +0700

    add(day10, aout): magic square

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,12 @@
 import Matrix.*
+import Arrays.*
 @main
 def main(): Unit = {
 
-  val rows = 1
-  val cols = 1
-  val rStart = 0
-  val cStart = 0
-  spiralMatrixIII(rows, cols, rStart, cStart).foreach(
-    elem => println(s"[${elem.mkString(", ")}]")
-  )
+
+  val grid = Array(Array(10,3,5), Array(1,6,11), Array(7,9,2))
+
+  println(numMagicSquaresInside(grid))
 }
 
 
